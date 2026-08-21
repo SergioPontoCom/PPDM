a0472a913a811e4cc426bfe7be07d0388f3b7534
SergioPontoCom <robson.s2008@aluno.ifsc.edu.br>
2026-08-21 11:42:12 -0300

    contador true

--- app/src/main/java/com/example/appdesergio/MainActivity.java
+++ app/src/main/java/com/example/appdesergio/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.appdesergio;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int contador=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -20,5 +25,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button b=findViewById(R.id.Button);
+        TextView tv=findViewById(R.id.TextView);
+
+        b.setOnClickListener(view -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
+
+
+        });
+
+
+
+
     }
 }
